7036da8387e4ddb1210b005cc2dc2ba70df56eed
Pras Velagapudi <psigen@gmail.com>
2015-04-22 21:40:23 -0400

    Added initial commit from R3 test repo.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dartpy
+# dartpy #
+
 Python bindings for the Dynamic Animation and Robotics Toolkit
+
+### Authors ###
+@mkoval and @psigen
+
+Developed at the Personal Robotics Laboratory at Carnegie Mellon.
---
+++ src/python.cpp
@@ -0,0 +1,19 @@
+#include <boost/python.hpp>
+#include <boost/eigen_numpy.h>
+
+void python_DartLoader();
+void python_DegreeOfFreedom();
+void python_SimWindow();
+void python_Skeleton();
+void python_World();
+
+BOOST_PYTHON_MODULE(PROJECT_NAME)
+{
+    SetupEigenConverters();
+
+    python_DartLoader();
+    python_DegreeOfFreedom();
+    python_SimWindow();
+    python_Skeleton();
+    python_World();
+}
