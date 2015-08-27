bc669ea04aea9b3afa19cd40301bd15f6201a498
Michael Koval <mkoval@cs.cmu.edu>
2015-08-27 13:08:30 -0400

    Finished wrapping Skeleton.

--- src/python_Skeleton.cpp
+++ src/python_Skeleton.cpp
@@ -2,19 +2,26 @@
 #include <boost/format.hpp>
 #include <boost/python.hpp>
 #include <dart/dynamics/dynamics.h>
+#include <dart/renderer/RenderInterface.h>
 #include "util.h"
 
 using ::dart::math::AngularJacobian;
 using ::dart::math::LinearJacobian;
 using ::dart::math::Jacobian;
+using ::dart::math::SupportPolygon;
 using ::dart::common::Subject;
 using ::dart::dynamics::BodyNode;
 using ::dart::dynamics::BodyNodePtr;
+using ::dart::dynamics::EndEffector;
+using ::dart::dynamics::EndEffectorPtr;
+using ::dart::dynamics::SoftBodyNodePtr;
 using ::dart::dynamics::DegreeOfFreedom;
 using ::dart::dynamics::DegreeOfFreedomPtr;
 using ::dart::dynamics::Joint;
 using ::dart::dynamics::Frame;
 using ::dart::dynamics::JointPtr;
+using ::dart::dynamics::WholeBodyIK;
+using ::dart::dynamics::PointMass;
 using ::dart::dynamics::JacobianNode;
 using ::dart::dynamics::Marker;
 using ::dart::dynamics::MetaSkeleton;
@@ -87,6 +94,23 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getAngularJacobianDeriv_overloads2,
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getIndexOf_overloads,
     MetaSkeleton::getIndexOf, 1, 2);
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(enableSelfCollision_overloads,
+    Skeleton::enableSelfCollision, 0, 1);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getRootBodyNode_overloads,
+    Skeleton::getRootBodyNode, 0, 1);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(computeForwardKinematics_overloads,
+    Skeleton::computeForwardKinematics, 0, 3);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(computeInverseDynamics_overloads,
+    Skeleton::computeInverseDynamics, 0, 3);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(draw_overloads,
+    Skeleton::draw, 0, 3);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(drawMarkers_overloads,
+    Skeleton::drawMarkers, 0, 3);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getIk_overloads,
+    Skeleton::getIk, 0, 1);
+BOOST_PYTHON_FUNCTION_OVERLOADS(create_overloads,
+    Skeleton::create, 0, 1);
+
 } // namespace
 
 namespace dart {
@@ -98,8 +122,10 @@ void python_Skeleton()
     using ::boost::noncopyable;
 
     typedef return_by_smart_ptr<BodyNodePtr> return_BodyNodePtr;
+    typedef return_by_smart_ptr<SoftBodyNodePtr> return_SoftBodyNodePtr;
     typedef return_by_smart_ptr<DegreeOfFreedomPtr> return_DegreeOfFredomPtr;
     typedef return_by_smart_ptr<JointPtr> return_JointPtr;
+    typedef return_by_smart_ptr<EndEffectorPtr> return_EndEffectorPtr;
 
     collection_from_python<std::vector<SkeletonPtr> >();
 
@@ -395,51 +421,240 @@ void python_Skeleton()
 
     class_<Skeleton, SkeletonPtr, bases<MetaSkeleton>, noncopyable>(
             "Skeleton", no_init)
-        .add_property("is_enabled_self_collision_check",
+        // Constructor and Destructor
+        .def("create",
+            static_cast<SkeletonPtr (*)(const std::string&)>(
+                &Skeleton::create),
+            create_overloads())
+        .def("create", 
+            static_cast<SkeletonPtr (*)(const Skeleton::Properties&)>(
+                &Skeleton::create))
+        .staticmethod("create")
+        .def("getMutex", &Skeleton::getMutex,
+            return_value_policy<reference_existing_object>())
+        .def("clone", &Skeleton::clone)
+
+        // Properties
+        .def("setProperties", &Skeleton::setProperties)
+        .def("getSkeletonProperties", &Skeleton::getSkeletonProperties,
+            return_value_policy<copy_const_reference>())
+        .def("setName", &Skeleton::setName,
+            return_value_policy<copy_const_reference>())
+        .def("getName", &Skeleton::getName,
+            return_value_policy<copy_const_reference>())
+        .def("enableSelfCollision", &Skeleton::enableSelfCollision,
+            enableSelfCollision_overloads())
+        .def("disableSelfCollision", &Skeleton::disableSelfCollision)
+        .def("isEnabledSelfCollisionCheck",
             &Skeleton::isEnabledSelfCollisionCheck)
-        .add_property("is_enabled_adjacent_body_check",
+        .def("isEnabledAdjacentBodyCheck",
             &Skeleton::isEnabledAdjacentBodyCheck)
-        .add_property("pose", &Skeleton_getPose, &Skeleton_setPose)
-        .add_property("num_rigid_body_nodes", &Skeleton::getNumRigidBodyNodes)
-        .add_property("num_soft_body_nodes", &Skeleton::getNumSoftBodyNodes)
-        .def("__init__", make_constructor(&Skeleton_constructor))
-        .def("get_root_body_node",
-            static_cast<BodyNode *(Skeleton::*)(size_t)>(
+        .def("setMobile", &Skeleton::setMobile)
+        .def("isMobile", &Skeleton::isMobile)
+        .def("setTimeStep", &Skeleton::setTimeStep)
+        .def("getTimeStep", &Skeleton::getTimeStep)
+        .def("setGravity", &Skeleton::setGravity)
+        .def("getGravity", &Skeleton::getGravity,
+            return_value_policy<copy_const_reference>())
+
+        // Structural Properties
+        // TODO: Wrap createJointAndBodyNodePair
+        .def("getNumRigidBodyNodes", &Skeleton::getNumRigidBodyNodes)
+        .def("getNumSoftBodyNodes", &Skeleton::getNumSoftBodyNodes)
+        .def("getNumTrees", &Skeleton::getNumTrees)
+        .def("getRootBodyNode",
+            static_cast<BodyNode* (Skeleton::*)(size_t)>(
                 &Skeleton::getRootBodyNode),
-            return_value_policy<return_BodyNodePtr>())
-        .def("get_soft_body_node",
-            static_cast<BodyNode *(Skeleton::*)(size_t)>(
+            return_value_policy<return_BodyNodePtr>(),
+            getRootBodyNode_overloads())
+        .def("getSoftBodyNode",
+            static_cast<SoftBodyNode* (Skeleton::*)(size_t)>(
+                &Skeleton::getSoftBodyNode),
+            return_value_policy<return_SoftBodyNodePtr>())
+        .def("getSoftBodyNode",
+            static_cast<SoftBodyNode* (Skeleton::*)(const std::string&)>(
+                &Skeleton::getSoftBodyNode),
+            return_value_policy<return_SoftBodyNodePtr>())
+        .def("getBodyNode",
+            static_cast<BodyNode* (Skeleton::*)(const std::string&)>(
                 &Skeleton::getBodyNode),
             return_value_policy<return_BodyNodePtr>())
-        .def("get_soft_body_node_by_name",
-            static_cast<SoftBodyNode *(Skeleton::*)(std::string const &)>(
-                &Skeleton::getSoftBodyNode),
-            return_value_policy<reference_existing_object>())
-        .def("get_joint_by_name",
-            static_cast<Joint *(Skeleton::*)(std::string const &)>(
+        .def("getTreeBodyNodes",
+            static_cast<const std::vector<BodyNode*>& (Skeleton::*)(size_t)>(
+                &Skeleton::getTreeBodyNodes),
+            return_value_policy<copy_const_reference>())
+            // TODO: Convert the output to a list of BodyNodePtrs.
+        .def("getJoint",
+            static_cast<Joint* (Skeleton::*)(const std::string&)>(
                 &Skeleton::getJoint),
             return_value_policy<return_JointPtr>())
-        .def("get_dof_by_name",
-            static_cast<DegreeOfFreedom *(Skeleton::*)(std::string const &)>(
+        .def("getDof",
+            static_cast<DegreeOfFreedom* (Skeleton::*)(const std::string&)>(
                 &Skeleton::getDof),
             return_value_policy<return_DegreeOfFredomPtr>())
-        .def("get_body_node_by_name",
-            static_cast<BodyNode *(Skeleton::*)(std::string const &)>(
-                &Skeleton::getBodyNode),
-            return_value_policy<return_BodyNodePtr>())
-        .def("get_marker_by_name",
-            static_cast<Marker *(Skeleton::*)(std::string const &)>(
+        .def("getTreeDofs",
+            static_cast<const std::vector<DegreeOfFreedom*>& (Skeleton::*)(size_t)>(
+                &Skeleton::getTreeDofs),
+            return_value_policy<copy_const_reference>())
+            // TODO: Convert the output to a list of DegreeOfFreedomPtrs.
+        .def("getNumEndEffectors", &Skeleton::getNumEndEffectors)
+        .def("getEndEffector",
+            static_cast<EndEffector* (Skeleton::*)(size_t)>(
+                &Skeleton::getEndEffector),
+            return_value_policy<return_EndEffectorPtr>())
+        .def("getEndEffector",
+            static_cast<EndEffector* (Skeleton::*)(const std::string&)>(
+                &Skeleton::getEndEffector),
+            return_value_policy<return_EndEffectorPtr>())
+        .def("getIK",
+            static_cast<const std::shared_ptr<WholeBodyIK>& (Skeleton::*)(bool)>(
+                &Skeleton::getIK),
+            return_value_policy<copy_const_reference>(),
+            getIk_overloads())
+        .def("createIK", &Skeleton::createIK,
+            return_value_policy<copy_const_reference>())
+        .def("clearIK", &Skeleton::clearIK)
+        .def("getMarker",
+            static_cast<Marker* (Skeleton::*)(const std::string&)>(
                 &Skeleton::getMarker),
             return_value_policy<reference_existing_object>())
-        .add_property("is_mobile", &Skeleton::isMobile, &Skeleton::setMobile)
-        .add_property("timestep", &Skeleton::getTimeStep, &Skeleton::setTimeStep)
-        .add_property("gravity",
-            make_function(&Skeleton::getGravity,
-                          return_value_policy<copy_const_reference>()),
-            &Skeleton::setGravity)
-        .def("enable_self_collision", &Skeleton::enableSelfCollision)
-        .def("disable_self_collision", &Skeleton::disableSelfCollision)
-        .def("compute_forward_kinematics", &Skeleton::computeForwardKinematics)
+            // TODO: Who owns this? Is there a MarkerPtr?
+
+        // Integration and finite difference
+        // TODO: These should not say "Documentation inherited."
+        .def("integratePositions", &Skeleton::integratePositions)
+        .def("integrateVelocities", &Skeleton::integrateVelocities)
+        .def("getPositionDifferences", &Skeleton::getPositionDifferences)
+        .def("getVelocityDifferences", &Skeleton::getVelocityDifferences)
+
+        // State
+        .def("setState", &Skeleton::setState)
+        .def("getState", &Skeleton::getState)
+
+        // Support Polygon
+        .def("getSupportPolygon",
+            static_cast<const SupportPolygon& (Skeleton::*)() const>(
+                &Skeleton::getSupportPolygon),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportPolygon",
+            static_cast<const SupportPolygon& (Skeleton::*)(size_t) const>(
+                &Skeleton::getSupportPolygon),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportIndices",
+            static_cast<const std::vector<size_t>& (Skeleton::*)() const>(
+                &Skeleton::getSupportIndices),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportIndices",
+            static_cast<const std::vector<size_t>& (Skeleton::*)(size_t) const>(
+                &Skeleton::getSupportIndices),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportAxes",
+            static_cast<const std::pair<Eigen::Vector3d,
+                    Eigen::Vector3d>& (Skeleton::*)() const>(
+                &Skeleton::getSupportAxes),
+            return_value_policy<copy_const_reference>())
+            // TODO: Convert the std::pair to a Python tuple.
+        .def("getSupportAxes",
+            static_cast<const std::pair<Eigen::Vector3d,
+                    Eigen::Vector3d>& (Skeleton::*)(size_t) const>(
+                &Skeleton::getSupportAxes),
+            return_value_policy<copy_const_reference>())
+            // TODO: Convert the std::pair to a Python tuple.
+        .def("getSupportCentroid",
+            static_cast<const Eigen::Vector2d& (Skeleton::*)() const>(
+                &Skeleton::getSupportCentroid),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportCentroid",
+            static_cast<const Eigen::Vector2d& (Skeleton::*)(size_t) const>(
+                &Skeleton::getSupportCentroid),
+            return_value_policy<copy_const_reference>())
+        .def("getSupportVersion",
+            static_cast<size_t (Skeleton::*)() const>(
+                &Skeleton::getSupportVersion))
+        .def("getSupportVersion",
+            static_cast<size_t (Skeleton::*)(size_t) const>(
+                &Skeleton::getSupportVersion))
+
+        // Kinematics algorithms
+        // TODO: This should be a Doxygen header.
+        .def("computeForwardKinematics", &Skeleton::computeForwardKinematics,
+            computeForwardKinematics_overloads())
+        
+        // Dynamics algorithms
+        // TODO: This should be a Doxygen header.
+        .def("computeForwardDynamics", &Skeleton::computeForwardDynamics)
+        .def("computeInverseDynamics", &Skeleton::computeInverseDynamics,
+            computeInverseDynamics_overloads())
+  
+        // Impulse-based dynamics algorithms
+        // TODO: This should be a Doxygen header.
+        .def("clearConstraintImpulses", &Skeleton::clearConstraintImpulses)
+        .def("updateBiasImpulse",
+            static_cast<void (Skeleton::*)(BodyNode*)>(
+                &Skeleton::updateBiasImpulse))
+        .def("updateBiasImpulse",
+            static_cast<void (Skeleton::*)(BodyNode*, const Eigen::Vector6d&)>(
+                &Skeleton::updateBiasImpulse))
+        .def("updateBiasImpulse",
+            static_cast<void (Skeleton::*)(BodyNode*, const Eigen::Vector6d&,
+                                           BodyNode*, const Eigen::Vector6d&)>(
+                &Skeleton::updateBiasImpulse))
+        .def("updateBiasImpulse",
+            static_cast<void (Skeleton::*)(SoftBodyNode*, PointMass*,
+                                           const Eigen::Vector3d&)>(
+                &Skeleton::updateBiasImpulse))
+        .def("updateVelocityChange", &Skeleton::updateVelocityChange)
+        .def("setImpulseApplied", &Skeleton::setImpulseApplied)
+        .def("isImpulseApplied", &Skeleton::isImpulseApplied)
+        .def("computeImpulseForwardDynamics",
+            &Skeleton::computeImpulseForwardDynamics)
+
+        // Equations of Motion
+        .def("getMassMatrix",
+            static_cast<const Eigen::MatrixXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getMassMatrix),
+            return_value_policy<copy_const_reference>())
+        .def("getAugMassMatrix",
+            static_cast<const Eigen::MatrixXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getAugMassMatrix),
+            return_value_policy<copy_const_reference>())
+        .def("getInvMassMatrix",
+            static_cast<const Eigen::MatrixXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getInvMassMatrix),
+            return_value_policy<copy_const_reference>())
+        .def("getInvAugMassMatrix",
+            static_cast<const Eigen::MatrixXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getInvAugMassMatrix),
+            return_value_policy<copy_const_reference>())
+        .def("getCoriolisForces",
+            static_cast<const Eigen::VectorXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getCoriolisForces),
+            return_value_policy<copy_const_reference>())
+        .def("getGravityForces",
+            static_cast<const Eigen::VectorXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getGravityForces),
+            return_value_policy<copy_const_reference>())
+        .def("getCoriolisAndGravityForces",
+            static_cast<const Eigen::VectorXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getCoriolisAndGravityForces),
+            return_value_policy<copy_const_reference>())
+        .def("getExternalForces",
+            static_cast<const Eigen::VectorXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getExternalForces),
+            return_value_policy<copy_const_reference>())
+        .def("getConstraintForces",
+            static_cast<const Eigen::VectorXd& (Skeleton::*)(size_t) const>(
+                &Skeleton::getConstraintForces),
+            return_value_policy<copy_const_reference>())
+        .def("notifyArticulatedInertiaUpdate",
+            &Skeleton::notifyArticulatedInertiaUpdate)
+        .def("notifySupportUpdate",
+            &Skeleton::notifySupportUpdate)
+
+        // Rendering
+        // TODO: This should be a Doxygen header.
+        .def("draw", &Skeleton::draw, draw_overloads())
+        .def("drawMarkers", &Skeleton::drawMarkers, drawMarkers_overloads())
         ;
 }
 
