e1019493583c1d904b98ab3f09421ddffccfb3e4
Michael Koval <mkoval@cs.cmu.edu>
2015-09-05 17:29:52 -0400

    Cleaned up property parsing.

---
+++ all_dart.h
@@ -0,0 +1,5 @@
+#include <dart/config.h>
+#include <dart/common/common.h>
+#include <dart/dynamics/dynamics.h>
+#include <dart/math/math.h>
+#include <dart/optimizer/optimizer.h>
--- parse2.py
+++ parse2.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from collections import namedtuple
+from tempfile import NamedTemporaryFile
 import argparse
 import logging
 import clang.cindex as ci
 import os.path
+import tempfile
 import yaml
 import sys
 
-# TODO: recursive property parsing is a bit of a mess
-# TODO: always pass down the root "properties" map and perform a full lookup
-
-# TODO: function arguments for user-defined types are not being fully qualified
+# TODO: constructors
 # TODO: default arguments
 # TODO: docstrings
 
@@ -44,12 +43,11 @@ def get_qualifiers(usr):
 
 
 class Declaration(object):
-    Properties = namedtuple('Properties', ['return_value_policy'])
-
     def __init__(self, cursor, parent=None):
         self.cursor = cursor
         self.parent = parent
         self.children = []
+        self.suppressed = False
 
     def __repr__(self):
         return '{declaration_name:s}({name:s})'.format(
@@ -59,6 +57,10 @@ class Declaration(object):
     @property
     def name(self):
         return self.cursor.spelling
+
+    @property
+    def key(self):
+        return self.qualified_name
 
     @property
     def qualified_name(self):
@@ -79,10 +81,19 @@ class Declaration(object):
     def is_volatile(self):
         return get_qualifiers(self.cursor.get_usr()).is_volatile
 
+    @classmethod
+    def validate_properties(cls, properties):
+        extra_keys = set(properties.keys()) - set(cls.PROPERTIES)
+        if extra_keys:
+            raise KeyError(
+                '{:s} has unknown keys: {:s}'.format(
+                    cls.__name__, sorted(extra_keys)))
+
 
 class Class(Declaration):
-    Properties = namedtuple('Properties', ['name', 'held_type', 'bases', 'noncopyable'])
-
+    PROPERTIES = ['name', 'held_type', 'bases', 'noncopyable']
+    Properties = namedtuple('Properties', PROPERTIES)
+        
     def __init__(self, cursor, parent):
         super(Class, self).__init__(cursor, parent)
         self.base_classes = []
@@ -124,8 +135,21 @@ class Class(Declaration):
 
         file.write('}\n')
 
+    def get_properties(self, properties):
+        self.validate_properties(properties)
+
+        return self.Properties(
+            name=properties.get('name', self.name),
+            held_type=properties.get('held_type', None),
+            bases=properties.get('bases', self.base_classes),
+            noncopyable=properties.get('noncopyable', False),
+        )
+
 
 class Enum(Declaration):
+    PROPERTIES = []
+    Properties = namedtuple('Properties', [])
+
     def __init__(self, cursor, parent):
         super(Enum, self).__init__(cursor, parent)
         self.values = []
@@ -138,9 +162,7 @@ class Enum(Declaration):
         )
 
     def emit(self, file):
-        # TODO: Implement this.
         # TODO: This will not work on C++-style Enum classes.
-
         file.write(
             '{{\n'
             '::boost::python::scope enum_scope = '
@@ -159,6 +181,9 @@ class Enum(Declaration):
 
 
 class Function(Declaration):
+    PROPERTIES = ['return_value_policy']
+    Properties = namedtuple('Properties', PROPERTIES)
+
     def __init__(self, cursor, parent):
         super(Function, self).__init__(cursor, parent)
 
@@ -179,12 +204,23 @@ class Function(Declaration):
         return [x.spelling for x in self.cursor.get_arguments()]
 
     @property
-    def signature(self):
+    def key(self):
         return '{result:s} {name:s}({arguments:s}){attributes:s}'.format(
             result=self.cursor.type.get_result().spelling,
-            name=self.name,
+            name=self.qualified_name,
             arguments=', '.join(
                 argument.type.spelling
+                for argument in self.cursor.get_arguments()
+            ),
+            attributes=' const' if self.is_const else '')
+
+    @property
+    def signature(self):
+        return '{result:s} {name:s}({arguments:s}){attributes:s}'.format(
+            result=self.cursor.type.get_result().get_canonical().spelling,
+            name=self.name,
+            arguments=', '.join(
+                argument.type.get_canonical().spelling
                 for argument in self.cursor.get_arguments()
             ),
             attributes=' const' if self.is_const else '')
@@ -197,16 +233,20 @@ class Function(Declaration):
             pointer_name = '(*)'
 
         return '{result:s} {name:s}({arguments:s}){attributes:s}'.format(
-            result=self.cursor.type.get_result().spelling,
+            result=self.cursor.type.get_result().get_canonical().spelling,
             name=pointer_name,
             arguments=', '.join(
-                argument.type.spelling
+                argument.type.get_canonical().spelling
                 for argument in self.cursor.get_arguments()
             ),
             attributes=' const' if self.is_const else ''
         )
 
     def emit(self, file):
+        # Optionally suppress the output of this instance.
+        if self.properties is None:
+            return
+
         # TODO: Why does get_ref_qualifier always return False?
         # TODO: Why does get_pointee() not return INVALID on references?
         returns_reference = (self.cursor.result_type.kind in [
@@ -223,9 +263,9 @@ class Function(Declaration):
                     self.properties.return_value_policy))
         elif returns_reference:
             logger.warning(
-                'Function "%s" with signature "%s" requires a'
-                ' "return_value_policy" to be specified because it returns a'
-                ' reference.', self.qualified_name, self.signature)
+                'Function "%s" requires a "return_value_policy" to be'
+                ' specified because it returns a' ' reference.',
+                self.key)
 
         # TODO: Add support for default arguments.
         argument_names = self.argument_names
@@ -245,8 +285,16 @@ class Function(Declaration):
         else:
             file.write('::boost::python::' + def_statement + ';\n')
 
+    def get_properties(self, properties):
+        self.validate_properties(properties)
+        return self.Properties(
+            return_value_policy=properties.get('return_value_policy', None))
+
 
 class Variable(Declaration):
+    PROPERTIES = []
+    Properties = namedtuple('Properties', PROPERTIES)
+
     def emit(self, file):
         # TODO: Implement this.
         file.write('// Variable.emit() is not implemented.\n')
@@ -397,7 +445,7 @@ def get_declarations_impl(cursor, parent, depth):
             raise ParserError(cursor,
                 'Found CXX_BASE_SPECIFIER outside of a CLASS_DECL.')
 
-        parent.base_classes.append(cursor.spelling)
+        parent.base_classes.append(cursor.type.get_canonical().spelling)
     elif cursor.kind in [ci.CursorKind.CXX_METHOD,
                          ci.CursorKind.FUNCTION_DECL]:
         recurse(Function(cursor, parent))
@@ -419,75 +467,18 @@ def get_declarations_impl(cursor, parent, depth):
     elif cursor.kind == ci.CursorKind.FUNCTION_TEMPLATE:
         return # TODO: Support function templates.
 
-def attach_default_properties(declaration, properties):
-    filtered_children = []
-
-    for child in declaration.children:
-        child.properties = properties.get(child.qualified_name, dict())
-        if child.properties is not None:
-            filtered_children.append(child)
-
-    declaration.children = filtered_children
-
-def attach_variable_properties(declaration, properties):
-    # There are currently no properties supported for variables.
-    declaration.properties = object()
-
-def attach_function_properties(declaration, properties):
-    declaration.properties = Function.Properties(
-        return_value_policy = properties.get('return_value_policy', None),
-    )
-
-    # TODO: Check for extraneous keys.
-
-def attach_class_properties(declaration, properties):
-    declaration.properties = Class.Properties(
-        name = properties.get('name', declaration.name),
-        held_type = properties.get('held_type', None),
-        bases = properties.get('bases', None),
-        noncopyable = properties.get('noncopyable', False),
-    )
-
-    # TODO: Check for extraneous keys.
-
-    method_properties = properties.get('methods', dict())
-    field_properties = properties.get('fields', dict())
-    filtered_children = []
-
-    for child in declaration.children:
-        if isinstance(child, (Class, Enum)):
-            filtered_children.append(child)
-            pass # TODO: Handle inner classes and enums.
-        elif isinstance(child, Function):
-            child.properties = method_properties.get(child.signature, dict())
-            if child.properties is not None:
-                attach_function_properties(child, child.properties)
-                filtered_children.append(child)
-        elif isinstance(child, Variable):
-            child.properties = field_properties.get(child.name, dict())
-            if child.properties is not None:
-                attach_variable_properties(child, child.properties)
-                filtered_children.append(child)
-        else:
-            raise ValueError(
-                'Unknown child "{:s}" of type "{:s}" on a Class.'.format(
-                    child, child.__class__.__name__))
-
-    declaration.children = filtered_children
-
-
 def attach_properties(declaration, properties):
-    attach_default_properties(declaration, properties)
+    this_properties= properties.get(declaration.key, dict())
+    declaration.suppressed = (this_properties is None)
+
+    if hasattr(declaration, 'get_properties'):
+        if this_properties is not None:
+            declaration.properties = declaration.get_properties(this_properties)
+        else:
+            declaration.properties = None
 
     for child in declaration.children:
-        if isinstance(child, Class):
-            attach_class_properties(child, child.properties)
-        elif isinstance(child, Function):
-            attach_function_properties(child, child.properties)
-        elif isinstance(child, Variable):
-            attach_variable_properties(child, child.properties)
-        else:
-            attach_properties(child, properties)
+        attach_properties(child, properties)
 
 
 def main():
@@ -507,6 +498,10 @@ def main():
 
     if 'compiler' in metadata:
         llvm_flags = metadata['compiler'].get('llvm_flags', ['-x', 'c++'])
+        if not isinstance(llvm_flags, list):
+            logger.fatal('Configuration option "compiler.flags" is not a list;'
+                         ' got "%s" instead.', type(llvm_flags).__name__)
+            return 1
 
         if 'llvm_path' in metadata['compiler']:
             ci.Config.set_library_path(metadata['compiler']['llvm_path'])
