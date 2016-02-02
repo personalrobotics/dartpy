4dbc2da1724a08bd2e8104facdfea0144c81a8e8
Michael Koval <mkoval@cs.cmu.edu>
2016-02-02 02:33:29 -0500

    Added a test for DartLoader.

---
+++ tests/test_DartLoader.py
@@ -0,0 +1,15 @@
+import dartpy
+import os.path
+import unittest
+
+class DartLoaderTest(unittest.TestCase):
+    ROOT_PATH = os.path.join(os.path.dirname(__file__), '..', 'test_resources')
+    URDF_PATH = os.path.join(ROOT_PATH, 'r2d2.urdf')
+
+    def test_parseSkeleton(self):
+        local_retriever = dartpy.LocalResourceRetriever()
+        loader = dartpy.DartLoader()
+
+        skeleton = loader.parseSkeleton(self.URDF_PATH, local_retriever)
+
+        self.assertIsNotNone(skeleton)
