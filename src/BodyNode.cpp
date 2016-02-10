e18cf177c6ef9378eb0680871f53ea3dee47894b
Michael Koval <mkoval@cs.cmu.edu>
2016-02-10 04:27:29 -0500

    Cleaned up multiplexer with type aliases.

--- src/BodyNode.cpp
+++ src/BodyNode.cpp
@@ -48,6 +48,54 @@ struct create_registry<Factory, Function, Arg, Args...>
   }
 };
 
+template <template <class> class Factory, class Function>
+struct JointMultiplexer;
+
+template <template <class> class Factory, class ReturnType, class... Args>
+struct JointMultiplexer<Factory, ReturnType (Args...)>
+{
+  using Function = ReturnType (Args...);
+
+  static Registry<Function> createRegistry()
+  {
+    using namespace dart::dynamics;
+
+    Registry<Function> registry;
+    create_registry<Factory, Function,
+        FreeJoint, PrismaticJoint, RevoluteJoint, ScrewJoint, WeldJoint,
+        UniversalJoint, BallJoint, EulerJoint, PlanarJoint, TranslationalJoint
+      >::execute(registry);
+
+    return registry;
+  }
+
+  static ReturnType execute(boost::python::object _jointType, Args... _args)
+  {
+    static Registry<Function> registry = createRegistry();
+
+    const auto it = registry.find(_jointType);
+    if (it == std::end(registry))
+    {
+      std::cerr << "Unsupported type for template argument." << std::endl;
+      return ReturnType();
+    }
+
+    return it->second(_args...);
+  }
+};
+
+using NominalJoint = dart::dynamics::WeldJoint;
+
+template <template <class> class Factory>
+using NominalJointMultiplexer = JointMultiplexer<
+  Factory, decltype(Factory<NominalJoint>::execute)>;
+
+} // namespace
+
+namespace dart {
+namespace python {
+
+//==============================================================================
 template <class JointType>
 struct BodyNode_moveTo2_factory
 {
@@ -68,46 +116,50 @@ struct BodyNode_moveTo2_factory
   }
 };
 
-template <template <class> class Factory, class ReturnType>
-struct Multiplexer
-{
-  template <class... Args>
-  static ReturnType execute(boost::python::object _jointType, Args... _args)
-  {
-    using namespace dart::dynamics;
-    using Function = ReturnType (Args...);
-
-    Registry<Function> registry;
-    create_registry<Factory, Function,
-        FreeJoint, PrismaticJoint, RevoluteJoint, ScrewJoint, WeldJoint,
-        UniversalJoint, BallJoint, EulerJoint, PlanarJoint, TranslationalJoint
-      >::execute(registry);
-
-    const auto it = registry.find(_jointType);
-    if (it == std::end(registry))
-    {
-      std::cerr << "Unsupported type for template argument." << std::endl;
-      return ReturnType();
-    }
-
-    return it->second(_args...);
-  }
-};
-
-} // namespace
-
-namespace dart {
-namespace python {
-
 dart::dynamics::Joint* BodyNode_moveTo2(
   dart::dynamics::BodyNode* _bodyNode,
   boost::python::object _jointType,
   dart::dynamics::BodyNode* _newParent,
   boost::python::object _jointProperties)
 {
-  return Multiplexer<BodyNode_moveTo2_factory, dart::dynamics::Joint*>::execute(
+  return NominalJointMultiplexer<BodyNode_moveTo2_factory>::execute(
     _jointType, _bodyNode, _newParent, _jointProperties);
 }
+
+//==============================================================================
+template <class JointType>
+struct BodyNode_moveTo3_factory
+{
+  static dart::dynamics::Joint* execute(
+    const dart::dynamics::SkeletonPtr &_newSkeleton,
+    dart::dynamics::BodyNode* _bodyNode,
+    dart::dynamics::BodyNode* _newParent,
+    boost::python::object _jointPropertiesPython)
+  {
+    typename JointType::Properties jointProperties;
+
+    if (!_jointPropertiesPython.is_none())
+    {
+      jointProperties = boost::python::extract<typename JointType::Properties>(
+        _jointPropertiesPython);
+    }
+
+    return _bodyNode->moveTo<JointType>(
+      _newSkeleton, _newParent, jointProperties);
+  }
+};
+
+dart::dynamics::Joint* BodyNode_moveTo3(
+  dart::dynamics::BodyNode* _bodyNode,
+  boost::python::object _jointType,
+  const dart::dynamics::SkeletonPtr& _newSkeleton,
+  dart::dynamics::BodyNode* _newParent,
+  boost::python::object _jointProperties)
+{
+  return NominalJointMultiplexer<BodyNode_moveTo3_factory>::execute(
+    _jointType, _newSkeleton, _bodyNode, _newParent, _jointProperties);
+}
+
 
 } // namespace python {
 } // namespace dart {
