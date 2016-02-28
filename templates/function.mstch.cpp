a72e0a465f10347416cd9f8a2550a049868878d3
Pras Velagapudi <psigen@gmail.com>
2016-02-27 20:21:39 -0500

    Fixed null arguments for static functions.

--- templates/function.mstch.cpp
+++ templates/function.mstch.cpp
@@ -15,8 +15,8 @@ void {{function.mangled_name}}()
 ::boost::python::scope parent_scope(parent_object);
 
 boost::python::def("{{function.name}}", {{!
-    }}static_cast<{{{function.type}}}>(&{{function.qualified_name}}), {{!
-    }}({{#function.params}}::boost::python::arg("{{name}}"){{^last}}, {{/last}}{{/function.params}}))
+    }}static_cast<{{{function.type}}}>(&{{function.qualified_name}}){{!
+    }}{{#function.params?}}, ({{#function.params}}::boost::python::arg("{{name}}"){{^last}}, {{/last}}{{/function.params}}){{/function.params?}})
 ;}
 {{{postcontent}}}
 {{{footer}}}
