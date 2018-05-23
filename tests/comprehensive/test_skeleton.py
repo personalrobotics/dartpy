7de24645cb882b9847e9a22eedd3d498175e8447
Jeongseok Lee <jslee02@gmail.com>
2018-05-23 14:11:29 -0700

    Add almost empty test_skeleton.py

---
+++ tests/comprehensive/test_skeleton.py
@@ -0,0 +1,14 @@
+import math
+
+import pytest
+from dartpy.utils import DartLoader
+from tests import get_asset_path
+from tests.util import get_skeleton_list
+
+
+def test_parse_skeleton_non_existing_path_returns_null():
+    skel_list = get_skeleton_list()
+
+
+if __name__ == "__main__":
+    pytest.main()
--- tests/util.py
+++ tests/util.py
@@ -1,10 +1,9 @@
-import inspect
 import os
 import os.path
 
 
-def get_path_to_this_file():
-    return inspect.getfile(inspect.currentframe())
+def get_current_file_path():
+    return os.path.dirname(__file__)
 
 
 def get_asset_path(rel_path, check_existing=False):
@@ -16,3 +15,28 @@ def get_asset_path(rel_path, check_existing=False):
         raise IOError("File %s does not exist" % full_path)
 
     return full_path
+
+
+def get_skeleton_list():
+    skeleton_list = []
+    skeleton_list += ['skel/chainwhipa.skel']
+    skeleton_list += ['skel/single_pendulum.skel']
+    skeleton_list += ['skel/single_pendulum_euler_joint.skel']
+    skeleton_list += ['skel/single_pendulum_ball_joint.skel']
+    skeleton_list += ['skel/double_pendulum.skel']
+    skeleton_list += ['skel/double_pendulum_euler_joint.skel']
+    skeleton_list += ['skel/double_pendulum_ball_joint.skel']
+    skeleton_list += ['skel/serial_chain_revolute_joint.skel']
+    skeleton_list += ['skel/serial_chain_eulerxyz_joint.skel']
+    skeleton_list += ['skel/serial_chain_ball_joint.skel']
+    skeleton_list += ['skel/serial_chain_ball_joint_20.skel']
+    skeleton_list += ['skel/serial_chain_ball_joint_40.skel']
+    skeleton_list += ['skel/simple_tree_structure.skel']
+    skeleton_list += ['skel/simple_tree_structure_euler_joint.skel']
+    skeleton_list += ['skel/simple_tree_structure_ball_joint.skel']
+    skeleton_list += ['skel/tree_structure.skel']
+    skeleton_list += ['skel/tree_structure_euler_joint.skel']
+    skeleton_list += ['skel/tree_structure_ball_joint.skel']
+    # skeleton_list += ['skelbody1.skel']
+
+    return skeleton_list
