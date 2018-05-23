24b963cb392b51441258209c6b3945ee6d8c3af1
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-05-22 22:15:22 -0700

    Setup CI testing for Xenial and Bionic using Docker (#57)

---
+++ .ci/script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+
+mkdir build
+cd build
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  cmake .. -DDARTPY_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION
+fi
+
+if [ "$TRAVIS_BRANCH" != "*-binding" ]; then
+  make binding
+fi
+
+# Don't actually build dartpy because the build time exceeds Travis CI time limit.
+# make -j4
+# $SUDO make install
+
+# Run pytest for Python tests of dartpy
+make pytest
