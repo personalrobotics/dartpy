448e2f6eedd18d7561318288b5254d0c9a7aa67f
Jeongseok Lee <jslee02@gmail.com>
2018-05-22 17:52:30 -0700

    Don't build dartpy; run python tests

--- .ci/script.sh
+++ .ci/script.sh
@@ -3,7 +3,19 @@ set -ex
 
 mkdir build
 cd build
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then cmake .. -DDARTPY_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION; fi
-if [ "$TRAVIS_BRANCH" != "*-binding" ]; then make binding; fi
-make -j4
-$SUDO make install
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  cmake .. -DDARTPY_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION
+fi
+
+if [ "$TRAVIS_BRANCH" != "*-binding" ]; then
+  make binding
+fi
+
+# Don't actually build dartpy because the build time exceeds Travis CI time limit.
+# make -j4
+# $SUDO make install
+
+# Run pytest for Python tests of dartpy
+# Install dartpy using apt-get instead
+sudo apt-get install python3-dartpy
+make pytest
