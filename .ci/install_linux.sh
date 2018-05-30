1aff10db3a376a82f3f4422a9de9ab6d2edb8d05
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-05-30 03:15:43 -0700

    Update install_linux.sh

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -16,7 +16,7 @@ sudo apt-get install python3-dev python3-numpy python3-boost-numpy-eigen # for P
 # Install Chimera
 if [ $(lsb_release -sc) = "trusty" ]; then
   # For installing chimera dependencies: llvm-3.7, llvm-3.7-tools, and libclang-3.7-dev
-  sudo add-apt-repository ppa:renemilk/llvm
+  sudo add-apt-repository ppa:renemilk/llvm -y
 fi
 sudo apt-get install chimera -y
 
