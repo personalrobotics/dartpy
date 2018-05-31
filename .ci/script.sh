c13bbec34efe06cae24efc90f538bf060824d7f5
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-05-31 10:39:16 -0700

    Update script.sh

--- .ci/script.sh
+++ .ci/script.sh
@@ -4,7 +4,7 @@ set -ex
 mkdir build
 cd build
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  cmake .. -DDARTPY_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION
+  cmake .. -DDARTPY_PYTHON_VERSION=$PYTHON_VERSION
 fi
 
 if [ "$TRAVIS_BRANCH" != "*-binding" ]; then
