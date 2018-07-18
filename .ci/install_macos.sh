b29d748179da1985ba95817fb1c08ccc876a6580
Jeongseok Lee <jslee02@gmail.com>
2018-07-17 22:05:27 -0700

    Enable cmake test on macOS

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -ex
 
-#brew install boost llvm@3.7
-#brew install yaml-cpp --with-static-lib
-#export PKG_CONFIG_PATH=/usr/local/Cellar/yaml-cpp/0.5.2/lib/pkgconfig 
+brew update > /dev/null
+
+# TODO: the list isn't complete
+brew install dartsim
+brew install boost
+brew install boost-python3
--- .ci/script.sh
+++ .ci/script.sh
@@ -7,20 +7,24 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   cmake .. -DDARTPY_PYTHON_VERSION=$PYTHON_VERSION
 fi
 
-if [ "$TRAVIS_BRANCH" != "*-binding" ]; then
+if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$TRAVIS_BRANCH" != "*-binding" ]; then
   make binding
 fi
 
-# Don't actually build dartpy because the build time exceeds Travis CI time limit.
-# make -j4
-# $SUDO make install
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  # Don't actually build dartpy because the build time exceeds Travis CI time limit.
+  # make -j4
+  # $SUDO make install
+fi
 
-# Run pytest for Python tests of dartpy
-which pytest
-pytest --version
-make pytest
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  # Run pytest for Python tests of dartpy
+  which pytest
+  pytest --version
+  make pytest
 
-cd $TRAVIS_BUILD_DIR
+  cd $TRAVIS_BUILD_DIR
 
-# Install dartpy as package name "dart"
-$SUDO pip3 install -e .
+  # Install dartpy as package name "dart"
+  $SUDO pip3 install -e .
+fi
