6b27e05ae4cfe586cdc713a048698058120395a5
Jeongseok Lee <jslee02@gmail.com>
2018-09-16 02:09:36 -0700

    Add pybind11 variable template

---
+++ chimera/pybind11/templates/variable.mstch.cpp
@@ -0,0 +1,21 @@
+{{header}}
+{{#includes}}
+#include <{{.}}>
+{{/includes}}
+{{#sources}}
+#include <{{.}}>
+{{/sources}}
+{{precontent}}
+#include <pybind11/pybind11.h>
+{{postinclude}}
+
+void {{variable.mangled_name}}(::pybind11::module& m)
+{
+    auto sm = m{{!
+        }}{{#variable.scope}}{{#name}}.attr("{{name}}"){{/name}}{{/variable.scope}};
+
+    sm.attr("{{variable.name}}") = {{variable.qualified_name}};
+}
+
+{{postcontent}}
+{{footer}}
